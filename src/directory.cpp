2357c84e704c3b9ec0a7cfa6cfde43781a794791
ether-lord <omnipresent.ether@yandex.ru>
2024-12-22 09:59:59 +1000

    memory mismanagement fix

--- src/directory.cpp
+++ src/directory.cpp
@@ -5,21 +5,21 @@
 
 using std::unique_ptr;
 
-std::string Directory::current_wd_ = "";
+std::string Directory::m_current_wd = "";
 
 Directory::Directory() {
 }
 
 const std::string& Directory::GetCurrent() {
-  if (current_wd_.empty()) {
+  if (m_current_wd.empty()) {
     size_t buff_size = 1024;
 
-    auto buffer = unique_ptr<char>(new char[buff_size]);
+    auto buffer = unique_ptr<char[]>(new char[buff_size]);
     getcwd(buffer.get(), buff_size);
 
     if (buffer != NULL)
-      current_wd_ = buffer.get();
+      m_current_wd = buffer.get();
   }
 
-  return current_wd_;
+  return m_current_wd;
 }
