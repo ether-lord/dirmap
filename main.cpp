5ca11f1c23ab69523597e8735f358abd2d4e56cd
ether-lord <omnipresent.ether@yandex.ru>
2024-12-22 11:18:01 +1000

    file size command line option

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
-#include <iostream>
+#include <unistd.h>
+
 #include <filesystem>
+#include <iostream>
 
 using namespace std;
 using namespace std::filesystem;
 
 int main(int argc, char** argv) {
-  auto directory_it = recursive_directory_iterator(current_path());
+  bool print_filesize = false;
 
-  for (const auto& file : directory_it)
-    cout << file.path().filename() << endl;
+  int rez = 0;
+  while ((rez = getopt(argc, argv, "s")) != -1) {
+    switch (rez) {
+      case 's':
+        print_filesize = true;
+        break;
+      default:
+        break;
+    }
+  }
+
+  auto directory_it = recursive_directory_iterator(current_path());
+  for (const auto& entry : directory_it) {
+    auto entry_str = entry.path().filename().string();
+
+    if (print_filesize && entry.is_regular_file())
+      entry_str.append("  --  ").append(to_string(entry.file_size())).append(" B");
+
+    cout << entry_str << endl;
+  }
 }
