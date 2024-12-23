03855bd12aa1a84a16a634e139bec404aba3bb57
ether-lord <omnipresent.ether@yandex.ru>
2024-12-23 13:21:46 +1000

    added function to format filesize

--- main.cpp
+++ main.cpp
@@ -1,32 +1,59 @@
 #include <unistd.h>
 
+#include <cmath>
 #include <filesystem>
 #include <iostream>
 
 using namespace std;
 using namespace std::filesystem;
 
+struct HumanReadable {
+  std::uintmax_t size{};
+
+ private:
+  friend std::ostream& operator<<(std::ostream& os, HumanReadable hr) {
+    int o{};
+    double mantissa = hr.size;
+    for (; mantissa >= 1024.; mantissa /= 1024., ++o);
+    os << std::ceil(mantissa * 10.) / 10. << "BKMGTPE"[o];
+    return o ? os << "B (" << hr.size << ") bytes" : os;
+  }
+};
+
 int main(int argc, char** argv) {
   bool print_filesize = false;
+  bool print_total_size = false;
+  bool print_filename = false;
 
   int rez = 0;
-  while ((rez = getopt(argc, argv, "s")) != -1) {
+  while ((rez = getopt(argc, argv, "sSf")) != -1) {
     switch (rez) {
       case 's':
         print_filesize = true;
+        break;
+      case 'S':
+        print_total_size = true;
+        break;
+      case 'f':
+        print_filename = true;
         break;
       default:
         break;
     }
   }
 
+  size_t total_files_size = 0;
   auto directory_it = recursive_directory_iterator(current_path());
   for (const auto& entry : directory_it) {
-    auto entry_str = entry.path().filename().string();
+    if (print_filename) {
+      auto entry_str = entry.path().filename().string();
+      cout << entry_str << endl;
+    }
 
-    if (print_filesize && entry.is_regular_file())
-      entry_str.append("  --  ").append(to_string(entry.file_size())).append(" B");
-
-    cout << entry_str << endl;
+    if (print_total_size && entry.is_regular_file())
+      total_files_size += entry.file_size();
   }
+
+  if (print_total_size)
+    cout << "Total size: " <<  HumanReadable{total_files_size} << endl;
 }
